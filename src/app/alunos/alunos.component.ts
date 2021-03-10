616c98c22bdb4d163fef42cc183c101f6e1475f6
Ary-Leal <yosttoleal@gmail.com>
2021-03-10 20:59:31 -0300

    commit utilizando ngfor com array no componente aluno

---
+++ src/app/alunos/alunos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos',
+  templateUrl: './alunos.component.html',
+  styleUrls: ['./alunos.component.css']
+})
+export class AlunosComponent implements OnInit {
+
+      titulo = 'Alunos';
+
+     public alunos = [
+     { nome: 'Ana'},
+     { nome: 'Amanda'},
+     { nome: 'Jack'},
+     { nome: 'Auristela'},
+     { nome: 'Arivaldo'},
+     { nome: 'Arinaldo'},
+     { nome: 'Ariosto'},
+
+];
+
+
+  constructor() {}
+
+
+
+  ngOnInit(): void {
+  }
+
+}
